d99f49a42fb27ff16dc0da8495f96a28aae134e7
tsrhayar <tahasrhayar1@gmail.com>
2022-05-18 17:45:58 +0100

    new commit

--- javascript/index.js
+++ javascript/index.js
@@ -8,7 +8,11 @@ $(document).ready(function () {
                         <div
                             class="col-4 col-lg-3 p-1 order-0 order-lg-0 d-flex border border-dark num-joueur"
                         >
-                            <input type="number" class="w-100 text-center" />
+                            
+                            <div class="row">
+                              <div class="col-4"><input class="ms-2 form-check-input me-2" type="radio" name="captitaine-${type}"></div>
+                              <div class="col-8"><input type="number" class="w-100 text-center" /></div>
+                            </div>
                         </div>
                         <div
                             class="col-12 col-lg-6 p-1 order-2 order-lg-1 d-flex border border-dark flex-column nom-joueur"
@@ -74,19 +78,42 @@ function rasOrReserve(name) {
 
     //
 
-    function rtlIfArabic(e) {
-      $(e).keyup(function () {
-        const arabic_regex = /[\u0600-\u06FF]/;
-        if (arabic_regex.test($(this).val())) {
-          $(this).attr("dir", "rtl");
-        } else {
-          $(this).attr("dir", "ltr");
-        }
-      });
-    }
     rtlIfArabic("#reserve-qualification-recevante");
     rtlIfArabic("#reserve-qualification-visiteuse");
   });
 }
 rasOrReserve("reserve-recevante");
 rasOrReserve("reserve-visiteuse");
+
+function rtlIfArabic(e) {
+  $(e).keyup(function () {
+    const arabic_regex = /[\u0600-\u06FF]/;
+    if (arabic_regex.test($(this).val())) {
+      $(this).attr("dir", "rtl");
+    } else {
+      $(this).attr("dir", "ltr");
+    }
+  });
+}
+
+$('input[name="reserve-technique"]').click(function () {
+  console.log($(this).val());
+  if ($(this).val() == "ras") {
+    $("#reserve-technique").html(`
+      <div class="row">
+        <div class="col-12 border border-dark reserve-technique-default d-flex">
+          <span class="display-1 m-auto">Ras</span>
+        </div>
+      </div>
+    `);
+  } else if ($(this).val() == "reserve") {
+    $("#reserve-technique").html(`
+      <div class="row">
+        <div class="col-12 border border-dark p-2 d-flex">
+          <textarea class="w-100" name="" rows="10" id="reserve-technique-input"></textarea>
+        </div>
+      </div>
+    `);
+  }
+  rtlIfArabic("#reserve-technique-input");
+});
